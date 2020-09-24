3393a80b5ccd8f72dc906ebc8e6452ca23b61366
Wojciech <wojciech.danowski@bpol.net>
2020-09-24 08:35:40 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { State } from "react-native-gesture-handler";
 
 export default function App() {
+  const [textValue, setTextValue] = useState("Steven CR?");
+
+  handleButtonClick = () => {
+    setTextValue("Adrian CR?");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{textValue}</Text>
+      <Button title="click me" onPress={() => handleButtonClick()}></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
